8ccc656bc72ad0e59d9a2a79d3a0943735d1e872
sweetCodex <61299169+thestorme212@users.noreply.github.com>
2024-10-01 21:07:44 +0100

    login setup+ invoices module

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 use App\Http\Controllers\AdminController;
-
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Auth.login');
 });
 Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/{page}', [AdminController::class, 'index']);
